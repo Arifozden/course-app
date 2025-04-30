54616463f3f87654266e94472fa4e692aee0073b
arifozden <arifozden1@gmail.com>
2025-05-01 00:54:57 +0200

    faq

--- src/pages/help/FaqPage.jsx
+++ src/pages/help/FaqPage.jsx
@@ -1,10 +1,21 @@
 export default function FaqPage() {
     return (
       <>
-      
-      <div id="faq">
-        <h1>Faq Page</h1>
-        <p>Welcome to the About page!</p>
-        </div></>
+        <div id="faq">
+        <h2>FAQs</h2>
+        <div className="question">
+          <h4>What is this website about?</h4>
+          <p>This website is a platform for users to ask questions and get answers from the community.</p>  
+        </div>  
+        <div className="question">
+          <h4>What is this website about?</h4>
+          <p>This website is a platform for users to ask questions and get answers from the community.</p>  
+        </div>  
+        <div className="question">
+          <h4>What is this website about?</h4>
+          <p>This website is a platform for users to ask questions and get answers from the community.</p>  
+        </div>  
+        </div>
+      </>
     );
   }

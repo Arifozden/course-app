eee08e4cb1334466339b2799f7c9551bd510fbdd
arifozden <arifozden1@gmail.com>
2025-05-01 00:03:05 +0200

    nav

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,14 +1,8 @@
-import { NavLink } from "react-router"
-
 export default function AboutPage() {
   return (
     <>
-    <nav>
-    <NavLink to="/">Home</NavLink>
-      <NavLink to="/about">About</NavLink>  
-      <NavLink to="/courses">Courses</NavLink>
-    </nav>
-    <div>
+    
+    <div id="about">
       <h1>About Page</h1>
       <p>Welcome to the About page!</p>
       </div></>
--- src/pages/Courses.jsx
+++ src/pages/Courses.jsx
@@ -1,13 +1,8 @@
-import { NavLink } from "react-router"
 export default function CoursesPage() {
   return (
     <>
-    <nav>
-    <NavLink to="/">Home</NavLink>
-      <NavLink to="/about">About</NavLink>  
-      <NavLink to="/courses">Courses</NavLink>
-    </nav>
-    <div>
+    
+    <div id="courses">
       <h1>Courses Page</h1>
       <p>Welcome to the Courses page!</p>
       </div>

073b975399552ffbe33fc381b0220547441102e1
arifozden <arifozden1@gmail.com>
2025-05-01 00:21:37 +0200

    help

---
+++ src/layouts/HelpLayout.jsx
@@ -0,0 +1,11 @@
+import { NavLink, Outlet } from "react-router";
+export default function HelpLayout() {
+    return (
+        <div id="help-layout">
+        <h1>Help Layout</h1>
+        <main>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Labore, inventore. Modi, praesentium vel! Mollitia eaque blanditiis et adipisci. Perferendis molestiae voluptatibus, quis temporibus aspernatur atque at obcaecati cumque esse? Esse.</p>
+    </main>
+        </div>
+    );
+    }

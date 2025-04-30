73dce61cbd614c45cc3cf80b5f4ca7533b628f07
arifozden <arifozden1@gmail.com>
2025-04-30 23:52:42 +0200

    nav link

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,10 +1,12 @@
+import { Link } from "react-router"
+
 export default function AboutPage() {
   return (
     <>
     <nav>
-      <a href="/">Home</a>
-      <a href="/about">About</a>  
-      <a href="/courses">Courses</a>
+      <Link to="/">Home</Link>
+      <Link to="/about">About</Link>  
+      <Link to="/courses">Courses</Link>
     </nav>
     <div>
       <h1>About Page</h1>
--- src/pages/Courses.jsx
+++ src/pages/Courses.jsx
@@ -2,9 +2,9 @@ export default function CoursesPage() {
   return (
     <>
     <nav>
-      <a href="/">Home</a>
-      <a href="/about">About</a>  
-      <a href="/courses">Courses</a>
+    <Link to="/">Home</Link>
+      <Link to="/about">About</Link>  
+      <Link to="/courses">Courses</Link>
     </nav>
     <div>
       <h1>Courses Page</h1>

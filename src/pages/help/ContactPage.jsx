0c7cf256513eaf5f5b437a4c7ffa60c0a022e2e7
arifozden <arifozden1@gmail.com>
2025-05-01 00:58:19 +0200

    contact

--- src/pages/help/ContactPage.jsx
+++ src/pages/help/ContactPage.jsx
@@ -3,8 +3,20 @@ export default function ContactPage() {
       <>
       
       <div id="contact">
-        <h1>Contact Page</h1>
-        <p>Welcome to the About page!</p>
+        <h2>Contact</h2>
+        <form>
+          <div>
+            <label htmlFor="email">
+              Email:</label>
+            <input type="email" id="email" name="email" required />
+              </div>
+              <div>
+            <label htmlFor="message">
+              Message:</label>
+            <textarea id="message" name="message" required></textarea>
+              </div>
+              <button type="submit">Submit</button>
+              </form>
         </div></>
     );
   }

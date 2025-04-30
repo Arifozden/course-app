e979a898e4da52d18b4cde6c16b2420e9e8acbd1
arifozden <arifozden1@gmail.com>
2025-05-01 00:00:33 +0200

    navnew

---
+++ src/layouts/MainLayout.jsx
@@ -0,0 +1,12 @@
+export default function MainLayout() {
+    return (
+        <div id="main-layout">
+        <h1>Main Layout</h1>
+        <nav>
+    <NavLink to="/">Home</NavLink>
+      <NavLink to="/about">About</NavLink>  
+      <NavLink to="/courses">Courses</NavLink>
+    </nav>
+        </div>
+    );
+    }
